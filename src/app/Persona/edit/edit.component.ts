7caec7342abadd3db85088f4dde07564d24046ea
Leonardo <ing.leovibaga16@gmail.com>
2019-06-11 21:00:17 -0500

    version1

---
+++ src/app/Persona/edit/edit.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Persona } from '../add/Modelo/Persona';
+import { Router } from '@angular/router';
+import { ServiceService } from 'src/app/Service/service.service';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+
+  persona: Persona = new Persona();
+  constructor(private router: Router, private service: ServiceService) { }
+
+  ngOnInit() {
+    this.Editar();
+  }
+
+  Editar() {
+    let id = localStorage.getItem("id");
+    this.service.getPersonaId(+id)
+      .subscribe(data => {
+        this.persona = data;
+      })
+  }
+  Actualizar(persona: Persona) {
+    this.service.updatePersona(persona)
+      .subscribe(data => {
+        this.persona = data;
+        alert("Se actualizo con éxito")
+        this.router.navigate(["listar"])
+      })
+  }
+
+}
